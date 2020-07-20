d34ffd77f6e5ced1b57b62d3d0b83866abb41d18
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-20 21:25:18 +0100

    create a list

---
+++ list.py
@@ -0,0 +1,14 @@
+# Create a list
+my_list = [2, 3, 4, 6, 8]
+
+# Access list items
+# Change the value of a list item
+# Loop through a list
+# Check if a list item exists
+# Get the length of a list
+# Add an item to the end of a list
+# Add an item at a specified index
+# Remove an item
+# Remove an item at a specified index
+# Empty a list
+# Use the list() constructor to make a list
