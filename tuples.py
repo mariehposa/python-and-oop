a553066d11e254ae8e181b95630564db4f53dc83
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-21 14:58:15 +0100

    Get the length of a tuple

--- tuples.py
+++ tuples.py
@@ -11,8 +11,10 @@ tuple_list[0] = 'cherry'
 # Loop through a tuple
 for x in my_tuple:
     print(x)
-    
+
 # Check if a tuple item exists
 # Get the length of a tuple
+print(len(my_tuple))
+
 # Delete a tuple
 # Use the tuple() constructor to create a tuple
