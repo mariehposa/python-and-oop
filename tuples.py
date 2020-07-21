e2d6fec9d07df9559cf954d99801cba0bb246d53
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-21 14:40:56 +0100

    Create a tuple

---
+++ tuples.py
@@ -0,0 +1,10 @@
+# Create a tuple
+my_tuple = (2, 3, 4, 5)
+
+# Access tuple items
+# Change tuple values
+# Loop through a tuple
+# Check if a tuple item exists
+# Get the length of a tuple
+# Delete a tuple
+# Use the tuple() constructor to create a tuple
