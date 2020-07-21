d348eb4f3954b38ee0c189eaed7bfcffbd49661b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-21 15:11:38 +0100

    Create a set

---
+++ sets.py
@@ -0,0 +1,14 @@
+# Create a set
+my_set = {'rice', 'maize', 'oat'}
+
+# Loop through a set
+# Check if an item exists
+# Add an item to a set
+# Add multiple items to a set
+# Get the length of a set
+# Remove an item in a set
+# Remove an item in a set by using the discard() method
+# Remove the last item in a set by using the pop() method
+# Empty a set
+# Delete a set
+# Use the set() constructor to create a set
