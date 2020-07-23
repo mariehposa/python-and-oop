2b95547af429b5c2ca92d015d0e60fbdd6496039
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-23 18:41:49 +0100

    create class for car owner

--- parking_lot.py
+++ parking_lot.py
@@ -1,10 +1,14 @@
 class ParkingLot:
-    def __init__ (self, name, car_name, isAdmin = False):
+    def __init__ (self, name, isAdmin = False):
         self.name = name
-        self.car_name = car_name
         self.isAdmin = isAdmin
 
-class Handler:
+class CarHandler:
     def __init__(self, name):
         super.__init__(name)
         self.cars_in_lot = []
+
+class CarOwnner:
+    def __init__(self, name, car_name):
+        super.__init__(name)
+        self.car_name = car_name
