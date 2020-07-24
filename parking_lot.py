502a4f7b64136bb910fa9d04e1e5a81252ac1dc4
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-24 16:27:50 +0100

    get cars

--- parking_lot.py
+++ parking_lot.py
@@ -10,11 +10,10 @@ class CarHandler(ParkingLot):
         super.__init__(name)
         self.cars_in_lot = []
 
-    def getCars(self, id, car_name, car_model, carOwnner):
-        self.id = uuid.uuid1()
-        self.car_name = car_name
-        self.car_model = car_model
-        self.carOwnner = carOwnner
+    def getCars(self, id, car_name, car_model):
+        id = uuid.uuid1()
+        car = Car(car_name, car_model, car_color, self)
+        self.cars_in_lot.append(id, car)
 
 class CarOwnner(ParkingLot):
     def __init__(self, name, car_name):
