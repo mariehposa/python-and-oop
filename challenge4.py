b56e98ee64d15c046f6d320fe5d51b4e33ce78ca
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-27 18:42:56 +0100

    wip

--- challenge4.py
+++ challenge4.py
@@ -1,11 +1,18 @@
 class Challenge4:
-    def __init__(self, type, food):
-        self.type = type
+    def __init__(self, typ, food):
+        self.typ = typ
         self.food = food
 
-    def _setter(self, type, food):
+    def _setter(self, food):
         if food is None:
-            raise Exception ("Food cannot be null ")
+            raise Exception ("Food cannot be null")
+
+        self._food = food
     
     def _getter(self):
-        return "{} is a {}".format(self.food, self.type)
+        return "{} is a food".format(self.food)
+
+    food = property(_getter, _setter)
+
+var = Challenge4('cereal', 'beans')
+print(var)
