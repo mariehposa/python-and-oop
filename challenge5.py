5d66201c2f6273a4d83372ba20156e36fbb0582f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-28 17:21:56 +0100

    wip

--- challenge5.py
+++ challenge5.py
@@ -1,3 +1,7 @@
 class Challenge5:
     def __init__(self, name):
         self.name = name
+
+    @classmethod
+    def get_name(cls, self):
+        print(f"Counter class has {cls.self.name} instances that have been created")
