511b607c910cfb4c5c12c4ac8485787f4e5eeb17
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-28 17:27:56 +0100

    define function to get name

--- challenge5.py
+++ challenge5.py
@@ -1,7 +1,13 @@
 class Challenge5:
+    count = 0
+
     def __init__(self, name):
+        Challenge5.count += 1
         self.name = name
 
+    def get_name(self):
+        return ('my name is {}'.format(self.name))
+
     @classmethod
-    def get_name(cls, self):
-        print(f"Counter class has {cls.self.name} instances that have been created")
+    def get_count(cls):
+        return (f"Challenge5 has {cls.count} class instances")
