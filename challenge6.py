80b170282addcd83c3b3b39535c6b51382a61963
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-29 23:15:06 +0100

    add objects

--- challenge6.py
+++ challenge6.py
@@ -1,4 +1,13 @@
 class Engineering:
-    def __init__(self, name, rank):
+    def __init__(self, name, rank, company):
         self.name = name
         self.rank = rank
+        self.company = company
+
+    def get_info(self):
+        print(f'{self.name} is a {self.rank} at {self.company}')
+
+class Software(Engineering):
+    def __init__(self, name, salary):
+        super().__init__(name)
+        self.salary =  salary
