c83b202e2aa5de1dab83ee5270003e66e2276fa9
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-07-30 14:22:43 +0100

    mvp done

--- challenge6.py
+++ challenge6.py
@@ -8,20 +8,24 @@ class Engineering:
         print(f'{self.name} is a {self.rank} at {self.company}')
 
 class Software(Engineering):
-    def __init__(self, name, salary):
-        super().__init__(name)
+    def __init__(self, name, rank, company, salary):
+        super().__init__(name, rank, company)
         self.salary =  salary
 
     def get_info(self):
         print(f'{self.name} salary as a software engineer is {self.salary}')
 
 class Hardware(Engineering):
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name, rank, company):
+        super().__init__(name, rank, company)
 
     def get_info(self):
         print(f'{self.name} is a hardware engineer')
 
 parent = Engineering('cassidy williams', 'senior developer', 'netlify')
-first_child = Software('angie jones', '$150000')
-second_child = Hardware('Yuken')
+first_child = Software('angie jones', 'java developer', 'app', '$150000')
+second_child = Hardware('Yuken', 'engineer', 'intel')
+
+parent.get_info()
+first_child.get_info()
+second_child.get_info()
