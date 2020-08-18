fa81a6575abb5b8433c2fe4601bc75b865d220cc
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-18 19:24:24 +0100

    define add method

--- linkedlist.py
+++ linkedlist.py
@@ -3,6 +3,17 @@ class Element:
         self.value = value
         self.next = None
 
-class Linkedlist:
+class Linkedlist(Element):
     def __init__(self, head):
         self.head = head
+
+    def add(self, new_value):
+        current_value = self.head
+
+        if self.head:
+            while current_value:
+                current_value = current_value.next
+            current_value.append(new_value)
+        else:
+            self.head = new_value
+        
