672a7b0c38d70f6689bcde4f055e0bf2c6829f07
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-19 10:34:20 +0100

    wip

--- linkedlist.py
+++ linkedlist.py
@@ -7,17 +7,18 @@ class Linkedlist(Element):
     def __init__(self, head=None):
         self.head = head
 
-    def add(self, new_value):
+    def append(self, new_value):
         current_value = self.head
 
         if self.head:
             while current_value.next:
                 current_value = current_value.next
-            current_value = new_value
+            current_value.next = new_value
         else:
             self.head = new_value
-
-# el1 = Element(1)
-# el2 = Element(2)
-# li = Linkedlist(3)
-# print(li.add(el2))
+            
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        return None
