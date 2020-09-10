6d9a0d37d2354d937f16afc4f2bf9c5a9ec08131
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-09-10 23:53:36 +0100

    wip

--- kangaroo.py
+++ kangaroo.py
@@ -8,4 +8,6 @@ import sys
 def kangaroo(x1, v1, x2, v2):
     if(x2 > x1 and v2 == v1):
         return('NO')
+    elif(x2 - x1) % (v2 - v1) == 0 and (x2 - x1) * (v2 - v1) < 0:
+        return('YES')
     
