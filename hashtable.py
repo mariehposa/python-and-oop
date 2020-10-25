aca034734616b53af81e6e456b0ccc5dc65de2d6
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-25 18:07:13 +0100

    add a method to check string index

--- hashtable.py
+++ hashtable.py
@@ -11,3 +11,12 @@ class HashTable(object):
           else:
             self.table[index].append(string)
         return -1
+
+    def lookup(self, string):
+        index = self.calculate_hash_value(string)
+
+        if index != None:
+          if self.table[index] != None:
+            if string in self.table[index]:
+              return index
+        return -1
